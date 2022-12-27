bc9a32642578bf8daf8502ae7912f145223bf783
dianavbet <diana.velasquez@itstarksolutions.com>
2022-12-26 20:37:45 -0600

    visa customer

---
+++ demoProject/src/main/java/phptravelsPageObjectRepository/PTCustomerPageObject.java
@@ -0,0 +1,81 @@
+package phptravelsPageObjectRepository;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotInteractableException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class PTCustomerPageObject {
+	WebDriver driver;
+
+	public PTCustomerPageObject(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	//-----------------------------------VISA -------- Diana
+	@FindBy(xpath="//*[@href='https://phptravels.net/visa']") WebElement visaBtn;
+	@FindBy(id="select2-from_country-container") WebElement fromCountryBtn;
+	@FindBy(id="select2-to_country-container") WebElement toCountryBtn;
+	@FindBy (id="date") WebElement selectDateBox;
+	public WebElement getVisaBtn() {
+		return visaBtn;
+	} 
+	public void clickVisaBtn() {
+		getVisaBtn().click();
+		//WebDriverWait waitElement = new WebDriverWait(driver, Duration.ofSeconds(10), Duration.ofSeconds(5));
+		//waitElement.until(ExpectedConditions.visibilityOfElementLocated(By.id("ACCOUNT")));
+
+		Wait<WebDriver> wait2 = new FluentWait<WebDriver>(this.driver).withTimeout(Duration.ofSeconds(60))
+				.pollingEvery(Duration.ofSeconds(3)).ignoring(ElementNotInteractableException.class);
+		wait2.until(ExpectedConditions.attributeToBe(By.xpath("//div[@id='preloader']"), "style",
+				"display: none;"));
+
+	}
+	//---SELECT FROM COUNTRY---- 
+	public WebElement getFromCountryBtn() {
+		return fromCountryBtn;
+	} 
+	public void clickFromCountry() {
+		getFromCountryBtn().click();
+	}
+	
+	public void selectFromCountry() {
+		List <WebElement> ListFromCountry =driver.findElements(By.xpath("//*[@id='select2-from_country-results']//li")); ;
+		ListFromCountry.get(5).click();
+	}
+	//------SELECT TO COUNTRY
+	public WebElement getToCountryBtn() {
+		return toCountryBtn;
+	} 
+		
+	public void clickToCountryBtn() {
+		getToCountryBtn().click();
+	}
+	public void selectToCountry() {
+		List <WebElement> ListToCountry =driver.findElements(By.xpath("//*[@id='select2-to_country-results']/li")); ;	
+		ListToCountry.get(5).click();
+	}
+	//-------SELECT DATE BOX -----
+	public WebElement getDateBox() {
+		return selectDateBox;
+	}
+	public void selectDateBox() {
+		getDateBox().click();
+	}
+	
+	
+	
+	//-----------------------------------END VISA -------- Diana
+
+	
+	
+}
