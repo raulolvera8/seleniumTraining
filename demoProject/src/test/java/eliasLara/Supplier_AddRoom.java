0a34130810a373e80c655f8048266343ff176c34
EliasLara66 <elias.lara@itstarksolutions.com>
2023-01-03 09:14:34 -0600

    Added comments in package eliasLara.

--- demoProject/src/test/java/eliasLara/Supplier_AddRoom.java
+++ demoProject/src/test/java/eliasLara/Supplier_AddRoom.java
@@ -10,32 +10,48 @@ import phptravelsPageObjectRepository.PTHomePage;
 import phptravelsPageObjectRepository.PTLoginPage;
 import phptravelsPageObjectRepository.PTSupplier_Page;
 
-public class Supplier_AddRoom extends Driver{
-	
+public class Supplier_AddRoom extends Driver {
+
 	WebDriver driver;
-	
+
 	@BeforeClass
 	public void setup() {
-		this.driver = initChromeDriver();	
+		this.driver = initChromeDriver();
 	}
-	
+
 	@Test
 	public void Supplier_AddRoomScript() {
-		
+
+		// Page object instances.
 		PTHomePage homePage = new PTHomePage(driver);
 		PTLoginPage loginPage = new PTLoginPage(driver);
 		PTSupplier_Page supplierPage = new PTSupplier_Page(driver);
-		
+
+		// Account selection, main page.
 		homePage.clickBtnAccount();
+
+		// Click in Supplier login, main page.
 		homePage.clickSupplierLoginbtn();
+
+		// Insert email textbox, login supplier page.
 		loginPage.setEmailtxtbox("supplier@phptravels.com");
+
+		// Insert email textbox, login supplier page.
 		loginPage.setPasswordtxtbox("demosupplier");
+
+		// Click in login button.
 		loginPage.clickLoginBtn();
+		
+		//Click in first level Hotels Module.
 		supplierPage.clickHotelsModuleBtn();
+		
+		//Click in second level Hotels Module > Hotels.
 		supplierPage.clickHotelsModuleNestedBtn();
+		
+		//Click in third level Hotels Module > Hotels > Add Room.
 		supplierPage.clickAddRoomsBtn();
 	}
-	
+
 	@AfterClass
 	public void teardown() {
 		System.out.println("The test script has passed successfully.");
--- demoProject/src/test/java/eliasLara/Supplier_CreateTour.java
+++ demoProject/src/test/java/eliasLara/Supplier_CreateTour.java
@@ -23,26 +23,60 @@ public class Supplier_CreateTour extends Driver {
 	
 	@Test
 	public void Supplier_CreateTourScript() {
+		
+		//Page object instances.
+		
 		PTHomePage homePage = new PTHomePage(driver);
 		PTLoginPage loginPage = new PTLoginPage(driver);
 		PTSupplier_Page supplierPage = new PTSupplier_Page(driver);
 		
 		
+		// Account selection, main page.
 		homePage.clickBtnAccount();
+		
+		//Click in Supplier login, main page.
 		homePage.clickSupplierLoginbtn();
+		
+		//Insert email textbox, login supplier page.
 		loginPage.setEmailtxtbox("supplier@phptravels.com");
+		
+		//Insert email textbox, login supplier page.
 		loginPage.setPasswordtxtbox("demosupplier");
+		
+		//Click in login button.
 		loginPage.clickLoginBtn();
+		
+		//Click in first level Tours Module.
 		supplierPage.clickToursModuleBtn();
+		
+		//Click in second level, tours module > tours.
 		supplierPage.clickToursModuleNestedBtn();
+		
+		//Click in third level, tours module > tours > manage tours.
 		supplierPage.clickManageToursBtn();
+		
+		//Click in green button "Add".
 		supplierPage.clickAddTourBtn();
+		
+		//Insert the tour name in textbox.
 		supplierPage.insertTourNameTxt("Huatulco Trip.");
+		
+		//Insert the tour description.
 		supplierPage.insertTourDescTxt("This is a test, describing a Huatulco trip.");
+		
+		//Insert adult quantity.
 		supplierPage.insertAdultQtyTxt("2");
+		
+		//Insert the adult price (one person value).
 		supplierPage.insertAdultPriceTxt("250.00");
+		
+		//Insert trip location.
 		supplierPage.insertLocationTourTxt("Huatulco");
+		
+		// Click in "Inclusions" Tab.
 		supplierPage.clickInclusionsBtn();
+		
+		//Select the checkbox...
 		supplierPage.clickSelectAllCheckbox();
 		supplierPage.clickCabFacilitiesChkbox();
 		supplierPage.clickLocalTaxChkbox();
