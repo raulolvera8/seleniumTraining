1a33e8829274cf7f92ed2bfe97486865ea9f7b34
EliasLara66 <elias.lara@itstarksolutions.com>
2023-01-23 15:20:51 -0600

    Updated script for reptiles.

--- demoProject/src/main/java/JPetStorePO/JP_ReptilesDashboard.java
+++ demoProject/src/main/java/JPetStorePO/JP_ReptilesDashboard.java
@@ -1,6 +1,8 @@
 package JPetStorePO;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class JP_ReptilesDashboard {
@@ -9,7 +11,44 @@ public class JP_ReptilesDashboard {
 	public JP_ReptilesDashboard(WebDriver Driver) {
 		this.Driver = Driver;
 		PageFactory.initElements(Driver, this);
+	}
 
+	// ******************************************************
+	// Title: validateFirstColumn.
+	// Objective: Extracts the value from first column and shows it to the user.
+	// Author: Elías Lara.
+	// Date of creation/adding: 01 23 23
+	// *******************************************************
+
+	@FindBy(xpath = "(//tbody/tr//td[text()])[1]")
+	WebElement firstColumn;
+
+	public WebElement getFirstColumn() {
+		return firstColumn;
+	}
+
+	public void validateFirstColumn() {
+		String value = getFirstColumn().getText();
+		System.out.println("The value obtained in the first column is " + value + ".");
+	}
+
+	// ******************************************************
+	// Title: validateSecondColumn.
+	// Objective: Extracts the value from second column and shows it to the user.
+	// Author: Elías Lara.
+	// Date of creation/adding: 01 23 23
+	// *******************************************************
+
+	@FindBy(xpath = "(//tbody/tr//td[text()])[2]")
+	WebElement secondColumn;
+
+	public WebElement getSecondColumn() {
+		return secondColumn;
+	}
+
+	public void validateSecondColumn() {
+		String value = getFirstColumn().getText();
+		System.out.println("The value obtained in the first column is " + value + ".");
 	}
 
 }
--- demoProject/src/test/java/eliasLara/PetStore_ReptilesValidations.java
+++ demoProject/src/test/java/eliasLara/PetStore_ReptilesValidations.java
@@ -1,6 +1,7 @@
 package eliasLara;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -22,6 +23,19 @@ public class PetStore_ReptilesValidations extends Driver {
 		//Page Object Instances.
 		JP_homePage homePage = new JP_homePage(driver);
 		JP_ReptilesDashboard reptilesDash = new JP_ReptilesDashboard(driver);
+		
+		//Click on reptiles icon.
+		homePage.clickReptilesIcon();
+		
+		//Extract the values from reptiles rows.
+		reptilesDash.getFirstColumn();
+		reptilesDash.getSecondColumn();
+	}
+	
+	@AfterClass
+	public void teardown() {
+		System.out.println("The test script has passed successfully.");
+		teardownDriver();
 	}
 
 }
