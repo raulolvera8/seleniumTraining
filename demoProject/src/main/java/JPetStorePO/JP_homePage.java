2bdd54b41dcef9ed5ade1c406e6d909304cb58f4
dianavbet <diana.velasquez@itstarksolutions.com>
2023-01-23 14:45:47 -0600

    DogsTab added

--- demoProject/src/main/java/JPetStorePO/JP_homePage.java
+++ demoProject/src/main/java/JPetStorePO/JP_homePage.java
@@ -1,5 +1,21 @@
 package JPetStorePO;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class JP_homePage {
+	WebDriver Driver;
+ public JP_homePage(WebDriver Driver) {
+	 this.Driver=Driver;
+	PageFactory.initElements(Driver, this);
+
+ }
+	
+// ---------- SELECT DOGS TAB ----------------- DIANA
+	// XPATHS FOR DOGS TAB
+@FindBy (xpath = "//*[@href='/actions/Catalog.action;jsessionid=3770D7D5B4516EE0D2E3E630D400835E?viewCategory=&categoryId=DOGS']" ) WebElement dogsTab;
+
 
 }
