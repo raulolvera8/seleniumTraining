07c735f1d58458e5913814e9c7fcb4eb8738a531
raulolvera8 <raul.olvera@outlook.com>
2023-01-24 12:50:04 -0600

    adding some files to raulOlvera package

---
+++ demoProject/src/test/java/raulOlvera/MultipleGroups.java
@@ -0,0 +1,26 @@
+package raulOlvera; 
+import org.testng.annotations.Test; 
+public class MultipleGroups 
+///RAUL OLVERA
+{ 
+@Test(groups={"Group1"}) 
+public void atom() 
+{ 
+  System.out.println("Smallest particle"); 
+} 
+@Test(groups = {"Group1", "Group2"}) // The test method "electron" belongs to two groups, Group1 and Group2. 
+public void electron()
+{ 
+  System.out.println("Negative charged particle"); 
+} 
+@Test(groups = {"Group2"}) 
+public void proton()
+{ 
+  System.out.println("Positive charged particle"); 
+} 
+@Test(groups = {"Group2"}) 
+public void neutron()
+{ 
+   System.out.println("Neutral particle"); 
+ } 
+}
