785de039025cedaef71cf35fd0a1337d9455c5b5
IrisRodriguezA <iris.rodriguez@itstarksolutions.com>
2023-01-30 09:59:03 -0600

    excel

--- demoProject/src/test/java/IrisRodriguez/scrip1.java
+++ demoProject/src/test/java/IrisRodriguez/scrip1.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
