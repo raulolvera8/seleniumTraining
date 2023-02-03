09f80f3922d3f2f2707584d7d23c6155714082e0
JuanJoseRE29 <juan.ramirez@itstarksolutions.com>
2023-02-03 00:53:03 -0600

    update listenersJuan

--- demoProject/src/main/java/library/listenersJuan.java
+++ demoProject/src/main/java/library/listenersJuan.java
@@ -1,5 +1,73 @@
 package library;
 
-public class listenersJuan {
+import org.testng.ISuiteListener;
+import org.testng.ITestListener;
+import org.testng.ISuite;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+
+public class listenersJuan implements ITestListener, ISuiteListener  {
+	
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the start of a test: ");		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		System.out.println("Results of this test: ");		
+
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This part of the test is failing: ");		
+
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		System.out.println("Test was skipped");		
+
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		System.out.println("");		
+
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		System.out.println("OnStart");		
+
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		System.out.println("OnFinish");		
+
+	}
+
+	@Override
+	public void onStart(ISuite suite) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFinish(ISuite suite) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
