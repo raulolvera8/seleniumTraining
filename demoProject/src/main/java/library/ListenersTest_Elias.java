bd6ea61da7633111521ec14129135b9a62c0faff
EliasLara66 <elias.lara@itstarksolutions.com>
2023-02-03 12:02:45 -0600

    Updated class ListenersTest_Elias

--- demoProject/src/main/java/library/ListenersTest_Elias.java
+++ demoProject/src/main/java/library/ListenersTest_Elias.java
@@ -12,13 +12,13 @@ public class ListenersTest_Elias implements ITestListener, ISuiteListener{
 	
 	@Override
 	public void onStart(ISuite suite) {
-		System.out.println("Suite main method started: " + suite);
+		System.out.println("Suite main method started: " + suite.getName());
 		
 	}
 
 	@Override
 	public void onFinish(ISuite suite) {
-		System.out.println("Suite main method finished: " + suite);
+		System.out.println("Suite main method finished: " + suite.getName());
 		
 	}
 	
@@ -26,42 +26,42 @@ public class ListenersTest_Elias implements ITestListener, ISuiteListener{
 
 	@Override //On Method test level.
 	public void onTestStart(ITestResult result) {
-		System.out.println("Commencing test execution: " + result);
+		System.out.println("Commencing test execution: " + result.getName());
 	}
 
 	@Override
 	public void onTestSuccess(ITestResult result) {
-		System.out.println("The test execution has finished successfully: " + result);
+		System.out.println("The test execution has finished successfully: " + result.getName());
 		
 	}
 
 	@Override
 	public void onTestFailure(ITestResult result) {
-		System.out.println("The test execution has finished failed: " + result);
+		System.out.println("The test execution has finished failed: " + result.getName());
 		
 	}
 
 	@Override
 	public void onTestSkipped(ITestResult result) {
-		System.out.println("The test execution has skipped some tests: " + result);
+		System.out.println("The test execution has skipped some tests: " + result.getName());
 		
 	}
 
 	@Override
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		System.out.println("The test execution has been completed partly: " + result);
+		System.out.println("The test execution has been completed partly: " + result.getName());
 		
 	}
 
 	@Override //Class Level.
 	public void onStart(ITestContext context) {
-		System.out.println("Class starting with tests to execute: " + context);
+		System.out.println("Class starting with tests to execute: " + context.getName());
 		
 	}
 
 	@Override //Class Level.
 	public void onFinish(ITestContext context) {
-		System.out.println("Class finishing with tests to execute: " + context);
+		System.out.println("Class finishing with tests to execute: " + context.getName());
 		
 	}
 
